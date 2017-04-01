b1fb7a7df722b45c8f88fafac778aeed48f2df85
6276431 <6276431@U707909.univali.br>
2017-04-01 11:38:59 -0300

    commit exercicio 3 - mais testes

--- src/exercicio3/TestesExercicio3.java
+++ src/exercicio3/TestesExercicio3.java
@@ -1,5 +1,55 @@
 package exercicio3;
 
-public class TestesExercicio3 {
+import static org.junit.Assert.*;
 
+import org.joda.time.IllegalFieldValueException;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.junit.Test;
+
+public class TestesExercicio3 {
+	
+	@Test
+	public void verificandoQueDoisEhDiferenteDeTres() throws Exception {
+		 assertEquals(3, 2);
+	}
+	
+	@Test
+	public void criarDataHoje() throws Exception {
+		 LocalDate hoje = new LocalDate(2017, 4, 1);
+		 assertEquals(1, hoje.getDayOfMonth());
+		 assertEquals(4, hoje.getMonthOfYear());
+		 assertEquals(2017, hoje.getYear());
+	}
+	
+	@Test
+	public void criarDataNascimento() throws Exception {
+		 LocalDate nascimento = new LocalDate(1979, 2, 22);
+		 assertEquals(22, nascimento.getDayOfMonth());
+		 assertEquals(2, nascimento.getMonthOfYear());
+		 assertEquals(1979, nascimento.getYear());
+	}		 
+	
+	@Test
+	public void criarHorarioAcordou() throws Exception {
+		LocalTime horario = new LocalTime(7, 52);
+		assertEquals(7, horario.getHourOfDay());
+		assertEquals(52, horario.getMinuteOfHour());		
+	}
+		
+	@Test(expected = IllegalFieldValueException.class)
+	public void criarDataHorarioErrado() throws Exception {
+		LocalDate data = new LocalDate(2017, 2, 29);		
+		LocalTime horario = new LocalTime(10, 57);		
+	}		
+	
+	@Test
+	public void somarDiaNaData() throws Exception {
+		LocalDate data = new LocalDate(2017, 2, 28).plusDays(1);			
+		assertEquals(1, data.getDayOfMonth());
+		assertEquals(3, data.getMonthOfYear());
+		assertEquals(2017, data.getYear());		
+	}		
+	
+	
 }
