de843530ceeee8d63a5f7c2fef115af9454bf717
6276431 <6276431@U707922.univali.br>
2017-04-08 11:49:46 -0300

    commit testes exercicio 5

--- src/exercicio5/TesteBancoAgenciaConta.java
+++ src/exercicio5/TesteBancoAgenciaConta.java
@@ -18,13 +18,15 @@ import br.ufsc.ine.leb.sistemaBancario.ValorMonetario;
 
 public class TesteBancoAgenciaConta {
 	
+	private SistemaBancario sistemaBancario;
 	private Banco bancoBrasil;
 	private Agencia bancoBrasilCentro;
 
 	
 	@Before
 	public void configurar() {		
-		bancoBrasil = new SistemaBancario().criarBanco("Banco do Brasil", Moeda.BRL);
+		sistemaBancario = new SistemaBancario(); 
+		bancoBrasil = sistemaBancario.criarBanco("Banco do Brasil", Moeda.BRL);
 		bancoBrasilCentro = bancoBrasil.criarAgencia("Centro");				
 	}
 
@@ -49,14 +51,11 @@ public class TesteBancoAgenciaConta {
 		assertTrue(mariaBancoBrasilCentro.calcularSaldo().zero());
 		assertEquals(bancoBrasilCentro, mariaBancoBrasilCentro.obterAgencia());
 	}
-
-
+	
 	@Test
 	public void mariaBancoBrasilCentroDeposito() throws Exception {
-		Conta mariaBancoBrasilCentro = bancoBrasilCentro.criarConta("Maria");
-		
-		Entrada deposito = new Entrada(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 10, 0)); 			
-		mariaBancoBrasilCentro.adicionarTransacao(deposito);
+		Conta mariaBancoBrasilCentro = bancoBrasilCentro.criarConta("Maria");		
+		sistemaBancario.depositar(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 10, 0));
 		
 		ValorMonetario valorMonetario = new ValorMonetario(Moeda.BRL);
 		valorMonetario.somar(new Dinheiro(Moeda.BRL, 10, 0));
@@ -65,18 +64,32 @@ public class TesteBancoAgenciaConta {
 		assertEquals("Maria", mariaBancoBrasilCentro.obterTitular());
 		assertEquals(valorMonetario, mariaBancoBrasilCentro.calcularSaldo());
 		assertEquals(bancoBrasilCentro, mariaBancoBrasilCentro.obterAgencia());
-	}
+	}	
 	
-
 	@Test
-	public void mariaBancoBrasilCentroSaque() throws Exception {
-		Conta mariaBancoBrasilCentro = bancoBrasilCentro.criarConta("Maria");
-
-		Entrada deposito = new Entrada(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 10, 0)); 			
-		mariaBancoBrasilCentro.adicionarTransacao(deposito);		
+	public void mariaBancoBrasilCentroSaque() throws Exception {		
+		Conta mariaBancoBrasilCentro = bancoBrasilCentro.criarConta("Maria");		
+		sistemaBancario.depositar(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 10, 0));
+		sistemaBancario.sacar(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 6, 0));		 			
 		
-		Saida saque = new Saida(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 6, 0));		 			
-		mariaBancoBrasilCentro.adicionarTransacao(saque);
+		ValorMonetario valorMonetario = new ValorMonetario(Moeda.BRL);
+		valorMonetario.somar(new Dinheiro(Moeda.BRL, 4, 0));
+
+		assertEquals("0001-5", mariaBancoBrasilCentro.obterIdentificador());
+		assertEquals("Maria", mariaBancoBrasilCentro.obterTitular());
+		assertEquals(valorMonetario, mariaBancoBrasilCentro.calcularSaldo());
+		assertEquals(bancoBrasilCentro, mariaBancoBrasilCentro.obterAgencia());
+	}		
+	
+	@Test
+	public void mariaBancoBrasilCentroSaldoNegativo() throws Exception {
+		Conta mariaBancoBrasilCentro = bancoBrasilCentro.criarConta("Maria");
+		
+		sistemaBancario.depositar(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 10, 0));
+		sistemaBancario.sacar(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 6, 0));		
+		
+		if (!mariaBancoBrasilCentro.calcularSaldo().negativo()) 		
+			sistemaBancario.sacar(mariaBancoBrasilCentro, new Dinheiro(Moeda.BRL, 6, 0));
 		
 		ValorMonetario valorMonetario = new ValorMonetario(Moeda.BRL);
 		valorMonetario.somar(new Dinheiro(Moeda.BRL, 4, 0));
@@ -86,7 +99,6 @@ public class TesteBancoAgenciaConta {
 		assertEquals(valorMonetario, mariaBancoBrasilCentro.calcularSaldo());
 		assertEquals(bancoBrasilCentro, mariaBancoBrasilCentro.obterAgencia());
 	}
-	
 	
 	
 }
